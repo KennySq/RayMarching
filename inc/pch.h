6e780109cf5e9b331023e86cf050c5dc2184c6e0
KennySq <odessa5d4@gmail.com>
2022-03-23 17:50:16 +0900

    ImGUI update

--- inc/pch.h
+++ inc/pch.h
@@ -1,4 +1,8 @@
 #pragma once
+#include"imgui.h"
+#include"imgui_impl_dx12.h"
+#include"imgui_impl_win32.h"
+
 #include<d3d12.h>
 #include<d3dx12.h>
 #include<dxgi1_6.h>
@@ -11,10 +15,13 @@
 #include<wrl.h>
 
 #include<assert.h>
+#include<atlbase.h>
 
 #include<iostream>
+#include<fstream>
 #include<memory>
 #include<vector>
+#include<filesystem>
 
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "d3dcompiler.lib")
