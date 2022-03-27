237b480a8ce6cf635bc51f8887a606dcd4c0e451
Ken <odessa5d4@gmail.com>
2022-03-27 19:14:53 +0900

    shader update

--- assets/hlsl/Essentials.hlsli
+++ assets/hlsl/Essentials.hlsli
@@ -1,15 +1,30 @@
 #define WIDTH 1280
 #define HEIGHT 720
 
-float sdfSphere(float3 center, float radius)
+float sdfCube(float3 samplePoint, float3 size)
 {
-    return length(center) - radius;
+	float3 d = abs(samplePoint) - size;
+
+	float inDistance = min(max(d.x, max(d.y, d.z)), 0.0f);
+	float outDistance = length(max(d, 0.0f));
+
+	return inDistance + outDistance;
+}
+
+float sdfSphere(float3 viewDir, float3 center, float radius)
+{
+    return distance(viewDir, center) - radius;
+}
+
+float sdfSphere(float3 samplePoint, float radius)
+{
+	return length(samplePoint) - radius;
 }
 
 float3 GetRayDirection(float3 view, float fov, float2 uv)
 {
-    float2 xy = uv - float2(WIDTH, HEIGHT) / 2.0f;
-    float focalLength = HEIGHT / tan(radians(fov)) / 2.0f;
+    float2 xy = view.xy + (uv - float2(WIDTH, HEIGHT) / 2.0f);
+    float focalLength = view.z + (HEIGHT / tan(radians(fov)) / 2.0f);
     
     return normalize(float3(xy, -focalLength));
 }
--- assets/hlsl/RayMarch_0.hlsl
+++ assets/hlsl/RayMarch_0.hlsl
@@ -1,14 +1,15 @@
 #define MAX_STEPS 256
-#define EPSILON 0.00001f
+#define EPSILON 0.001f
 #define MAX_DISTANCE 100.0f
 
-#include"Essentials.hlsli"
+#include "Essentials.hlsli"
 
 cbuffer Constants : register(b0)
-{    
-    float4x4 PaddingMatrix;
-    float FadeFactor;
+{
+	float4 Fade;
 }
+
+static const float3 gLightPosition = float3(-50, 100, -100);
 
 struct Pixel
 {
@@ -16,22 +17,71 @@ struct Pixel
     float2 Texcoord : TEXCOORD;
 };
 
+float sdfSmooth(float a, float b, float k)
+{
+	float res = exp(-k * a) + exp(-k * b);
+	return -log(max(0.0001, res)) / k;
+}
+
+float sdfScene(float3 samplePoint)
+{
+	float cube0 = sdfCube(samplePoint - float3(-Fade.x,0,0), 1.0f);
+	float sphere0 = sdfSphere(samplePoint, 1.0f);
+	return sdfSmooth(cube0, sphere0, 5.0f);
+}
+
+float3 ApproximateNormal(float3 samplePoint)
+{
+	float x = sdfScene(float3(samplePoint.x + EPSILON, samplePoint.y, samplePoint.z))
+						 - sdfScene(float3(samplePoint.x - EPSILON, samplePoint.y, samplePoint.z));
+	float y = sdfScene(float3(samplePoint.x, samplePoint.y + EPSILON, samplePoint.z))
+						 - sdfScene(float3(samplePoint.x, samplePoint.y - EPSILON, samplePoint.z));
+	float z = sdfScene(float3(samplePoint.x, samplePoint.y, samplePoint.z + EPSILON))
+						 - sdfScene(float3(samplePoint.x, samplePoint.y, samplePoint.z - EPSILON));
+
+	return float3(x, y, z);
+}
+
+float4x4 generateView(float3 eye, float3 at, float3 up)
+{
+	float3 f = normalize(at - eye);
+	float3 s = normalize(cross(f, up));
+	float3 u = cross(s, f);
+	
+	return float4x4(
+		float4(s,0),
+		float4(u,0),
+		float4(-f,0),
+		float4(0,0,0,1));
+}
+
 float march(float3 view, float3 uvw, float near, float far)
 {
     float depth = near;
-    
-    for (float i = 0; i < MAX_STEPS; i += 1.0f)
-    {
-        float distance = sdfSphere(view + (depth * uvw), 1.0f);
+	float theta = Fade.x;
+	float4x4 rotMat = float4x4(
+		float4(1,0,0,0),	
+		float4(0,cos(theta),-sin(theta),0),
+		float4(0,sin(theta),cos(theta),0),
+		float4(0,0,0,1)
+	);
 
+    for (int i = 0; i < MAX_STEPS; i++)
+    {
+		float3 viewDir = view + (depth * uvw);
+		float distance = sdfScene(viewDir);
+		float3 normal = ApproximateNormal(viewDir);
+		float3 lightDir = gLightPosition - float3(0,0,0);
+		float diffuse = saturate(dot(-lightDir, normal));
+		
         if (distance < EPSILON)
         {
-            return depth;
+            return diffuse;
         }
         
         depth += distance;
         
-        if (depth >= far)
+        if (depth > far)
         {
             return far;
         }
@@ -42,15 +92,17 @@ float march(float3 view, float3 uvw, float near, float far)
 
 float4 frag(Pixel input) : SV_Target0
 {
-    float3 view = float3(0, 0, 2);
+    float3 eye = float3(1, -1.5,1);
+	float4x4 viewMat = generateView(eye, float3(0,0,0), float3(0,1,0));
     float2 fragCoord = input.Texcoord * float2(WIDTH, HEIGHT);
-    float3 dir = GetRayDirection(view, 60.0f, fragCoord);
-    
-    float dist = march(view, dir, 0.01f, 100.0f);
+    float3 dir = GetRayDirection(eye, 60.0f, fragCoord);
+	float3 worldDir = mul(dir, viewMat);
+	
+    float dist = march(eye, worldDir, 0.01f, 100.0f);
     if (dist > MAX_DISTANCE - EPSILON)
     {
         return float4(0, 0, 0, 0);
     }
-    
-    return float4(1, 1, 1, 0);
+   
+    return float4(dist, dist, dist, 0);
 }
