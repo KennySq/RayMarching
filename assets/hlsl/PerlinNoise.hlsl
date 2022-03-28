37ff5bafdd709667858959e9224f53ef1bccfa7f
KennySq <odessa5d4@gmail.com>
2022-03-28 17:46:26 +0900

    PerlinNoise shader update

---
+++ assets/hlsl/PerlinNoise.hlsl
@@ -0,0 +1,28 @@
+struct Vertex
+{
+    float4 Position : POSITION0;
+    float2 Texcoord : TEXCOORD0;
+    int InstanceID : SV_InstanceID;
+};
+
+struct Pixel
+{
+    float4 Position : SV_Position;
+    float2 Texcoord : TEXCOORD;
+    int TargetIndex : SV_RenderTargetArrayIndex;
+};
+
+Pixel vert(Vertex input)
+{
+    Pixel output = (Pixel) 0;
+    output.Position = input.Position;
+    output.Texcoord = input.Texcoord;
+    output.TargetIndex = input.InstanceID;
+    
+    return output;
+}
+
+float4 frag(Pixel input) : SV_Target0
+{
+    return float4(1, 0, 0, 0);   
+}
